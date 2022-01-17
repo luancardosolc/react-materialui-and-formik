2fe7a1ed5bc7996bee0dc0231038e23f7cdd5bd1
Luan Cardoso <luanleitecardoso@gmail.com>
2022-01-17 19:16:50 -0300

    input example without formik

--- src/components/SingnUp.tsx
+++ src/components/SingnUp.tsx
@@ -1,12 +1,20 @@
 import { FormControl, FormHelperText, Input, InputLabel } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function SingnUp() {
+  const [firstName, setFirstName] = useState('Luan');
+
   return (
     <>
       <FormControl sx={{ backgroundColor: 'white', borderRadius: '10px' }}>
         <InputLabel htmlFor="firstName">First Name</InputLabel>
-        <Input id="firstName" name="firstName" type='text' />
+        <Input
+          id="firstName"
+          name="firstName"
+          type='text'
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
         <FormHelperText id="my-helper-text">Email msg.</FormHelperText>
       </FormControl>
     </>
